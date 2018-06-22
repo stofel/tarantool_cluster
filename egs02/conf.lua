5ec2e7f5eb3aaf76fc60179322979f31d304a5c1
Common user <general@devel.ximad.com>
2018-06-22 09:30:54 +0000

    example

--- egs02/conf.lua
+++ egs02/conf.lua
@@ -4,11 +4,13 @@
 --==========================================
 --= Config
 local conf = {
-    snapshot_period     = 60,
-    snapshot_count      = 2,
+    checkpoint_interval = 600,
+    checkpoint_count    = 2,
     rows_per_wal        = 10000,
+    read_only           = false,
+    --replication         = {'10.0.1.1:3311', '10.0.1.2:3311', '10.0.1.3:3311', '10.0.1.4:3311'},
+    replication         = '10.0.1.1:3311',
     listen              = '10.0.1.3:3311',
-    replication_source  = {'10.0.1.4:3311', '10.0.1.1:3311', '10.0.1.2:3311'}
   }
 return {box = conf, console_url = '10.0.1.3:3322'}
 --==========================================
