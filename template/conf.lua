08a53cf6cabcd64d76643adc353a966d59f12505
Pfls User <pfls@socialpuzzles2.ximad.com>
2018-06-30 06:53:10 +0000

    more pretty template

--- template/conf.lua
+++ template/conf.lua
@@ -1,12 +1,22 @@
 --==========================================
 --= Config
+db_ip   = '10.0.1.1'
+db_port = '3311'
+console_port = '3312'
+repl = {
+ '10.0.1.1:' .. db_port,
+ '10.0.1.2:' .. db_port,
+ '10.0.1.3:' .. db_port,  
+ '10.0.1.4:' .. db_port
+}
 local conf = {
     checkpoint_interval = 600,
     checkpoint_count    = 2,
     rows_per_wal        = 10000,
-    replication         = {'10.0.1.1:3311', '10.0.1.2:3311', '10.0.1.3:3311', '10.0.1.4:3311'},
-    listen              = '10.0.1.1:3311'
+    replication         = repl,
+    read_only           = false,
+    listen              = db_ip .. ':' .. db_port
   }
-return {box = conf, console_url = '10.0.1.1:3322'}
+return {box = conf, console_url = db_ip .. ':' .. console_port}
 --==========================================
 
